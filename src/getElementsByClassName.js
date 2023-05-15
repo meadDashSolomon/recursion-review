eeb65ae338288eb47e8faca5928cfbffce893eee
Kiel Fuller <kpfuller28@gmail.com>
2023-05-15 15:35:43 -0400

    pair work from 5/15

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, root = document.body
 ) {
-  // your code here
+  console.log(root);
+  var elements = [];
+
+  // base case
+  if (root.className && root.contains(className)) {
+    elements.push(root);
+  }
+
+  // OSS
+  root.childNodes.forEach(function (child) {
+    elements = elements.concat(getElementsByClassName(className, child));
+  });
+  console.log(elements);
+  return elements;
 };
